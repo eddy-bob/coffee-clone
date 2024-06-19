dc91cb0ed13329f3bb1c502d0ed4a29c0734be88
Madu Eddy <eddymadu90@gmail.com>
2024-06-19 23:59:09 +0100

    Update Select.tsx

--- src/components/forms/Select.tsx
+++ src/components/forms/Select.tsx
@@ -1,15 +1,5 @@
 import * as React from "react";
 
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-
 interface IProps {
   placeholder: string;
   item: { value: string; icon: string }[];
@@ -17,20 +7,6 @@ interface IProps {
 export function SelectEl(props: IProps) {
   const { placeholder = "Select element" } = props;
   return (
-    <Select className="rounded-lg shadow-lg p-12 text-16">
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder={placeholder} />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Fruits</SelectLabel>
-          <SelectItem value="apple">Apple</SelectItem>
-          <SelectItem value="banana">Banana</SelectItem>
-          <SelectItem value="blueberry">Blueberry</SelectItem>
-          <SelectItem value="grapes">Grapes</SelectItem>
-          <SelectItem value="pineapple">Pineapple</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+    <></>
   );
 }
